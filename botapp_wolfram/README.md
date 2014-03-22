5df1fbef281fa7a29ab393666c35c1885b13d57e
aisoy-robotics <pgarcia@aisoy.com>
2014-03-22 23:06:21 +0100

    added wolfram botapp

---
+++ botapp_wolfram/README.md
@@ -0,0 +1,25 @@
+INSTRUCTIONS
+============
+
+Install the following libraries
+* sudo apt-get update
+* sudo apt-get install wolfram-engine (not sure inf neccesary)
+* sudo apt-get install python-setuptools
+* sudo easy_install pip
+* sudo pip install wolframalpha
+
+You can use the given APP_ID while available, or get a free API key here http://products.wolframalpha.com/api/ 
+
+Choose the language you want to use
+LANGUAGE = Language.English #Language.Spanish, Language.French, etc...
+
+Copy botapp_wolfram.py script into your raspberrPi, e.g. in /home/pi
+
+Launch the script, e.g. python /home/pi/botapp_wolfram.py
+
+Make a question and the AISoy1 will give you a response.
+
+Enjoy!
+
+
+
---
+++ botapp_wolfram/botapp_wolfram.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import wolframalpha
+import sys
+import unicodedata 
+    
+import rospy
+import roslib; 
+roslib.load_manifest('aisoy_sdk_tts')
+roslib.load_manifest('aisoy_sdk_asr')
+roslib.load_manifest('aisoy_sdk_translate')
+roslib.load_manifest('aisoy_common')
+from libaisoy_sdk_tts import *;
+from libaisoy_sdk_asr import *;
+from libaisoy_sdk_translate import *;
+from libaisoy_common import *
+
+#You can get a free API key here http://products.wolframalpha.com/api/
+APP_ID='Q59EW4-7K8AHE858R'
+
+LANGUAGE = Language.English #Language.Spanish, Language.French, etc...
+
+####### WOLFRAM CLASS ##################################################
+class Wolfram:
+	def __init__(self):
+		self.client = wolframalpha.Client(APP_ID)
+		self.tts = Tts(TtsName.GoogleTts)
+		self.tts.setTtsLanguage(LANGUAGE)
+		self.asr = Asr(AsrName.GoogleAsr)
+		self.asr.setAsrLanguage(LANGUAGE)
+		self.translate = Translate()
+ 
+	def mainLoop(self):
+		while not rospy.is_shutdown():
+			rospy.loginfo("Waiting for user sentence........")
+			sentence = self.asr.listen()
+
+			if sentence != "":
+				rospy.loginfo("Question: %s",sentence)
+				if LANGUAGE != Language.English:
+					sentence = self.translate.translate(sentence, LANGUAGE, Language.English)
+
+				answer = self.process_sentence(sentence)
+
+				if LANGUAGE != Language.English:
+					answer = self.translate.translate(answer, Language.English, LANGUAGE)
+
+				rospy.loginfo("Answer: %s",answer)
+				self.tts.say(answer)
+
+	def process_sentence(self, sentence):
+		query = sentence
+		res = self.client.query(query)
+
+		if len(res.pods) > 0:
+			texts = ""
+			pod = res.pods[1]
+			if pod.text:
+				texts = ". ".join(elem for elem in self.toASCII(pod.text).split("\n"))
+				texts = texts.replace("|","")
+			else:
+			  texts = "I have no answer for that"
+			return texts
+		else:
+			return "I am not sure"
+
+	def toASCII(self, input_str):
+		nkfd_form = unicodedata.normalize('NFKD', unicode(input_str))
+		only_ascii = nkfd_form.encode('ASCII', 'ignore')
+		return only_ascii
+
+####### MAIN #############################################################
+if __name__ == '__main__':
+    wolfram = Wolfram()
+    rospy.init_node('wolfram_node')
+    wolfram.mainLoop()
